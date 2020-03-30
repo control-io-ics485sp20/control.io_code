def2e582722f776dac6cb283d6a5ab2db3f42f00
Barryn Chun (Chunmeista-YOGA) <barryn.chun@gmail.com>
2020-03-30 03:33:55 -1000

    renamed items to gameobjects

--- src/client/js/objects/gameobjects.js
+++ src/client/js/objects/gameobjects.js
@@ -1,4 +1,6 @@
-function Item(id) {
+//gameitem
+//base object
+function GameItem(id) {
     this.id = id;
     this.position.x = 0;
     this.position.y = 0;
@@ -6,8 +8,11 @@ function Item(id) {
     this.image = "";
 }
 
+//ship
+//controlled by player or ai
+//has hp
 function Ship(id) {
-    Item.call(this, id);
+    GameItem.call(this, id);
 
     //these are ideas. Not sure if our ships will use these statistics.
     this.health = 0;
@@ -15,21 +20,28 @@ function Ship(id) {
     this.fuel = 0;   //might not need fuel. 
 }
 
+//asteroid
+//has hp
 function Asteroid(id) {
-    Item.call(this, id);
+    GameItem.call(this, id);
 
     this.health = 0;
     this.damage = 0;
 }
 
+//space station
+//capturable
+//has hp
 function Station(id) {
-    Item.call(this, id);
+    GameItem.call(this, id);
 
     // this.health = 0;
     this.status = "unclaimed";
     this.owner = null;
 }
 
+//planet
+//capturable
 function Planet() {
-
+    GameItem.call(this, id);
 }
